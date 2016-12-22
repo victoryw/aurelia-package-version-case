aaec9ea73d9ec5bd4577ce8eb98455b6f57d74c8
tianyawy <tianya_wy@hotmail.com>
2016-12-22 20:29:05 +0800

    all

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+import {WebAPI} from './web-api';
+
 export class App {
+  static inject() { return [WebAPI]; }
+
+  constructor(api) {
+    this.api = api;
+  }
+
   configureRouter(config, router){
     config.title = 'Contacts';
     config.map([
