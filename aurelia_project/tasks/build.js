f7d898494c1fd14cc830092b2cf34b3a37f06c91
tianyawy <tianya_wy@hotmail.com>
2016-12-27 16:22:44 +0800

    [victoryw] auto update multple entry to rev js file

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -5,6 +5,13 @@ import processCSS from './process-css';
 import {build} from 'aurelia-cli';
 import project from '../aurelia.json';
 import revReplace from 'gulp-rev-replace'
+import fileFinder from 'fs-finder'
+import fs from 'fs'
+import find from 'find'
+import _ from 'underscore'
+import jsonfile from 'jsonfile'
+import path from 'path'
+
 
 export default gulp.series(
   readProjectConfiguration,
@@ -25,20 +32,46 @@ function writeBundles() {
   return build.dest();
 }
 
-function replaceJsIfMap(filename) {
-  return filename.replace('scripts/', '');
-}
-
 function updateIndexFileRef() {
-  var manifest = gulp.src("./rev-manifest.json");
+  let revFileLocation = path.join('./','scripts/rev-manifest.json');
+  var manifest = gulp.src(revFileLocation);
+  getBundleFileNames(revFileLocation);
 
   return gulp.src(["./index.html","./index2.html"])
     .pipe(revReplace({
-        manifest: manifest,
-        modifyUnreved: replaceJsIfMap,
-        modifyReved: replaceJsIfMap
+        manifest: manifest
     }))
-    .pipe(gulp.dest("./scripts"));
+    .pipe(gulp.dest("./"));
+}
+
+
+function getBundleFileNames(revFileLocation) {
+  let files = find.fileSync(/\.js$/,'./scripts');
+
+  var fileVecMappings = files.map(file => {
+    let names = file.split(/[-.]/); //the name should be app-bundle-c8a2963f94.js
+    ///TODO:app-bundle.js
+    return {
+      name : file.replace('scripts/',''),
+      source: file.replace('scripts/','').replace("-"+names[names.length - 2],""),
+      createTime: fs.statSync(file).mtime.getTime()
+    }});
+
+
+  // console.log(_.max(value,file=>file.createTime));
+  var mapping = _.object(
+    _.map(_.chain(fileVecMappings)
+      .groupBy(file=> {return file.source})
+      .map((value, key)=> {
+        return {
+          source : key,
+          fileRev: _.max(value, file=> {return file.createTime}).name
+        }
+      })
+      .value(),
+      _.values));
+
+  jsonfile.writeFileSync(revFileLocation, mapping);
 
 }
 
